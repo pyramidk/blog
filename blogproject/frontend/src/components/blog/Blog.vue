e8e3cbd8854abdbc69d5924067a0aadb889b7d68
pyramidk <zackbai1991@gmail.com>
2018-05-05 22:39:45 +0800

    feat: add sidebar

--- blogproject/frontend/src/components/blog/Blog.vue
+++ blogproject/frontend/src/components/blog/Blog.vue
@@ -1,17 +1,27 @@
 <template lang="pug">
-  div.blog__home 'home'
+  div.blog
+    Sidebar
+    div.blog_wrapper
+      BlogContent
 </template>
 
 <style lang="stylus" scoped>
-  .blog__home
-    height 100%
+  .blog
     width 100%
+    height 100%
+  .blog_wrapper
+    height 100%
+    margin-left 301px
+    background #f0f0f0
 </style>
 
 <script>
-
+import Sidebar from './Sidebar'
+import BlogContent from './BlogContent'
 export default {
   components: {
+    Sidebar,
+    BlogContent
   },
   data () {
     return {
---
+++ blogproject/frontend/src/components/blog/Sidebar.vue
@@ -0,0 +1,38 @@
+
+<template lang="pug">
+  div.siderbar
+    div.siderbar-title pyramid
+    div.siderbar-navigation
+      div.siderbar-navigation-item Home
+      div.siderbar-navigation-item Categories
+      div.siderbar-navigation-item Post Types
+    div.siderbar-footer © 2018 Pyra All Rights Reserved.
+</template>
+
+<style lang="stylus" scoped>
+  .siderbar
+    position fixed
+    top: 0
+    left 0
+    width 220px
+    height 100%
+    padding  60px 40px 0
+    font-family 'Josefin Sans", Arial, sans-serif'
+    color #777
+    border-right 1px solid #f0f0f0
+  .siderbar-navigation
+    margin-top 90px
+  .siderbar-navigation-item
+    margin-bottom 25px
+  .siderbar-footer
+    position fixed
+    bottom 40px
+    font-size 14px
+    letter-spacing 0.3px
+</style>
+
+<script>
+export default {
+
+}
+</script>
